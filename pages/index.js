c8e05a15b1927a1994fc47613255a7f5b2147d71
Megan Hill <meganhill.iowa@gmail.com>
2022-01-26 22:15:48 -0600

    Add Buggons

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {SudokuGrid} from "../components/SudokuGrid";
+import {useState} from "react";
 
-export const Home = () => (
-    <div className={styles.container}>
-        <Head>
-            <title>Sudoku Solver</title>
-            <meta name="description" content="Sudoku Puzzle Solver"/>
-            <link rel="icon" href="/favicon.ico"/>
-        </Head>
+export const Home = () => {
+    const [gridIsFinalized, setGridIsFinalized] = useState(false);
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Sudoku Solver</title>
+                <meta name="description" content="Sudoku Puzzle Solver"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-        <main className={styles.main}>
-            <h1 className={styles.title}>Sudoku Solver</h1>
-            <h2>Enter your Sudoku below then press Solve to solve it</h2>
+            <main className={styles.main}>
+                <h1 className={styles.title}>Sudoku Solver</h1>
+                <h2>Enter your Sudoku below then press Solve to solve it</h2>
 
-            <SudokuGrid />
+                <SudokuGrid/>
 
-            {/*<div className={styles.buttonRow}>*/}
-            {/*    <button>Enter New Puzzle</button>*/}
-            {/*    <button>Play New Puzzle</button>*/}
-            {/*    <button>Fill in Hints</button>*/}
-            {/*    <button>Solve Puzzle</button>*/}
-            {/*</div>*/}
-        </main>
-    </div>
-);
+                <div className={styles.buttonRow}>
+                    <button disabled={gridIsFinalized}>Finalize Your Grid</button>
+                    <button disabled={!gridIsFinalized}>Fill in Hints</button>
+                    <button disabled={!gridIsFinalized}>Solve Puzzle</button>
+                </div>
+            </main>
+        </div>
+    );
+}
 
 export default Home;
